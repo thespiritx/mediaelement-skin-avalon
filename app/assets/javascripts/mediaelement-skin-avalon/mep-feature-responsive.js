cd837549b1c2f8ebcdedc6d4e3b86775ec3ff462
phuongdh <phuongdh@gmail.com>
2013-11-11 13:06:23 -0500

    Fix for audio player skin
    Do not hide timestamps and slider on iPhone audio player

--- app/assets/javascripts/mediaelement-skin-avalon/mep-feature-responsive.js
+++ app/assets/javascripts/mediaelement-skin-avalon/mep-feature-responsive.js
@@ -1,7 +1,7 @@
 (function($) {
         $.extend(MediaElementPlayer.prototype, {
                 buildresponsive: function(player, controls, layers, media) {
-			if (mejs.MediaFeatures.isiPhone) {
+			if (mejs.MediaFeatures.isiPhone && player.isVideo) {
 	                        player.container.on("controlsready", function() {
         	                        player.total.parent().css('visibility', 'hidden');
                 	                player.currenttime.parent().css('visibility', 'hidden');
